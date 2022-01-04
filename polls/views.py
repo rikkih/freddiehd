8c2f56bce8665513be1818bec541a8e03e05ee26
Rikki Hodder <rikkih@protonmail.com>
2022-01-04 23:52:23 +0000

    adding in view templates

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,19 @@
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
     return HttpResponse("Hello, world. You're at the polls index")
+
+
+def detail(request: HttpRequest, question_id: int) -> HttpResponse:
+    return HttpResponse(f"You're looking at question {question_id}")
+
+
+def results(request: HttpRequest, question_id: int) -> HttpResponse:
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request: HttpRequest, question_id: int) -> HttpResponse:
+    return HttpResponse("You're voting on question %s." % question_id)
